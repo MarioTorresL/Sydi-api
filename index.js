608f06ef1d842d8912a59c39665730e4cdfcdfb0
nicorubio96 <nicolasrubioolivares96@gmail.com>
2022-08-04 21:40:58 -0400

    middlewares for developing response

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-
+const bodyParser = require('body-parser')
+const morgan = require('morgan');
+const responseTime = require('response-time')
 // Create Express
 const app = express();
 
+// middleware to format json response
+app.use(bodyParser.json({type:'application/json'}))
+// logging developer
+morgan('dev')
+
+// middlewere for time response
+responseTime()
 app.listen( process.env.PORT , ()=>{
   console.log('Runing server in Port 3000')
 });
+
