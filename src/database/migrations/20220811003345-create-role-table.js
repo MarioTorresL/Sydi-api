fd60d57b0c6b89db6cdac05dcb6204548f3c0ddb
Mario Torres <mario.torreslepe@gmail.com>
2022-08-10 22:16:12 -0400

    create user and roles models and migrations

---
+++ src/database/migrations/20220811003345-create-role-table.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.createTable('Roles',{
+      id:{
+        type:Sequelize.DataTypes.INTEGER,
+        autoIncremetn:true,
+        primaryKey:true
+      },
+      name:{
+        type:Sequelize.DataTypes.STRING,
+        allowNull:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DataTypes.DATE
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.dropTable('Roles');
+  }
+};
---
+++ src/database/migrations/20220811003415-create-user-table.js
@@ -0,0 +1,68 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.createTable('Users',{
+      id:{
+        type:Sequelize.DataTypes.INTEGER,
+        autoIncrement:true,
+        primaryKey:true
+      },
+      firstName:{
+        type:Sequelize.DataTypes.STRING,
+        allowNull:false
+      },
+      lastName:{
+        type:Sequelize.DataTypes.STRING,
+      },
+      email:{
+        type:Sequelize.DataTypes.STRING,
+        allowNull:false,
+        unique:true
+      },
+      password:{
+        type:Sequelize.DataTypes.STRING,
+        allowNull:false
+      },
+      image:{
+        type:Sequelize.DataTypes.BLOB
+      },
+      RoleId: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Roles',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DataTypes.DATE
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.dropTable('Users');
+  }
+};
